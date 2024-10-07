c79f5b581d0a3167878a03d33dc2ab73ab396f03
Raul Gomez <raul.gomez.3191@gmail.com>
2024-10-07 10:07:30 -0600

    Giving some modularity and structure to the code

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,14 @@ use regex::Regex;
 extern crate cosmos;
 use cosmos::*;
 
-
 // Error handling
-use anyhow::{anyhow, Context, Ok, Result};
+use anyhow::{anyhow, Ok, Result};
 
 // Logging
 extern crate log;
-use log::{debug, error, log_enabled, info, Level};
+use log::info;
 
 use std::str::FromStr;
-use crate::proto::cosmos::auth::v1beta1::BaseAccount;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -27,17 +25,25 @@ struct Args {
 }
 
 #[derive(Debug)]
-struct Txn{
+struct Txn {
     address: String,
     amount: u32,
     denom: String,
 }
 
+struct SenderInfo {
+    wallet: Wallet,
+    address: Address
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+
+    // Enable Logging
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
+    // Parse arguments
     let args = Args::parse();
 
     info!("Operation: {}", args.operation);
@@ -55,89 +61,92 @@ async fn main() {
     let address: String = args.address;
     let amount: u32 = caps["amount"].parse::<u32>().unwrap();
     let denom: String = caps["denom"].to_string();
-
-
+    
     let txn = Txn { address, amount, denom };
 
-    another_fn(txn).await;
-}
-
-async fn another_fn(txn: Txn) -> Result<(), anyhow::Error> {
-    // Execute the transaction
-
-    let chain: Cosmos = connect_to_chain().await?;
-
-    log::info!("Successfully connected to chain");
-
-    /////////////////////////////////////////////////
-    /// 
-    /// Get my data
-
-    const PHRASE: &str =
-            "again ready search face detail violin gesture pluck tuition dinner wealth debate exclude okay wait raven hawk gold dream myself bullet pitch barely tortoise";
-
-    let seed_phrase = SeedPhrase::from_str(PHRASE).unwrap();
-
-    let wallet = seed_phrase
-                            .with_hrp(AddressHrp::from_static("osmo"))
-                            .unwrap();
-
-    let sender_address_str = wallet.get_address_string();
-    info!("Raul's Wallet address is {}", sender_address_str);
-    // My Wallet: osmo1vmlnscnqv2wevdxczxl53dz0kmdwqdw4glhvdw
-
-    let sender_address = wallet.get_address();
-    let balances = cosmos::Cosmos::all_balances(&chain, sender_address).await?;
-
-    // Iterate over balances, for each
-    balances.iter().for_each(|balance| {
-        // Show and record info
-        if balance.denom == "uosmo" { 
-            info!("Raul's UOSMO balance: {} {}", balance.amount, balance.denom);
-        }
-    });
-
-    /////////////////////////////////////////////////
-
-    let receiver_address_str: std::result::Result<Address, error::AddressError> = Address::from_str(&txn.address);
-    let receiver_address = receiver_address_str?.get_address();
-
-    //let base_account: BaseAccount = chain.get_base_account(real_address).await?;
-
-    // Get all balances
-    let balances = cosmos::Cosmos::all_balances(&chain, receiver_address).await?;
-
-    // Iterate over balances, for each
-    balances.iter().for_each(|balance| {
-        // Show and record info
-        if balance.denom == "uosmo" { 
-            info!("Receiver's UOSMO balance: {} {}", balance.amount, balance.denom);
-        }
-    });
-
-    /////////////////////////////////////////////////
-
-    let amount_to_send: Vec<Coin> = vec![Coin{denom: txn.denom.to_owned(), amount: txn.amount.to_string()}];
-
-    wallet.send_coins(&chain, receiver_address, amount_to_send).await?;
-    
-    Ok(())
-
-    // match connect_to_chain().await {
-    //     Ok(_)=>{
-    //         log::info!("Successfully connected to chain");
-    //         Ok(())
-    //     }
-    //     Err(e)=>{
-    //         error!("{}", e);
-    //         return Err(e);
-    //     }
-    // }
+    main_execution(txn).await;
 }
 
 async fn connect_to_chain() -> Result<Cosmos, anyhow::Error> {
     // Connect to the blockchain
     let cosmos_handle = CosmosNetwork::OsmosisTestnet.connect().await?;
-    
+    log::info!("Successfully connected to chain");
+
     Ok(cosmos_handle)
+}
+
+async fn get_coin_balance(chain: &Cosmos, address: Address, denom: &str) -> Result<String, anyhow::Error> {
+    let balances: Vec<Coin> = cosmos::Cosmos::all_balances(&chain, address).await?;
+    let mut amount = "0";
+
+    balances.iter().for_each(|balance| {
+        if balance.denom == denom { 
+            amount = &balance.amount;
+        }
+    });
+
+    Ok(amount.to_string())
+}
+
+async fn get_sender_info() -> Result<SenderInfo, anyhow::Error> {
+    const PHRASE: &str =
+    "again ready search face detail violin gesture pluck tuition dinner wealth debate exclude okay wait raven hawk gold dream myself bullet pitch barely tortoise";
+
+    let seed_phrase = SeedPhrase::from_str(PHRASE).unwrap();
+
+    let wallet = seed_phrase
+                        .with_hrp(AddressHrp::from_static("osmo"))
+                        .unwrap();
+
+    // My Wallet: osmo1vmlnscnqv2wevdxczxl53dz0kmdwqdw4glhvdw
+    // let sender_address_str = wallet.get_address_string();
+    let sender_address: Address = wallet.get_address();
+
+    Ok(SenderInfo { wallet: wallet, address: sender_address })
+}
+
+async fn get_sender_balance(chain: &Cosmos, sender_address: &Address, txn: &Txn) -> Result<(), anyhow::Error> {
+    let amount = get_coin_balance(chain, *sender_address, &txn.denom).await?;
+    info!("Sender's balance: {} {}", amount, &txn.denom);
+    Ok(())
+}
+
+async fn get_recipient_address(txn: &Txn) -> Result<Address, anyhow::Error> {
+    let recipient_address_str: std::result::Result<Address, error::AddressError> = Address::from_str(&txn.address);
+    let recipient_address = recipient_address_str?.get_address();
+    Ok(recipient_address)
+}
+
+async fn get_recipient_balance(chain: &Cosmos, recipient_address: &Address, txn: &Txn) -> Result<(), anyhow::Error> {
+    let amount = get_coin_balance(chain, *recipient_address, &txn.denom).await?;
+    info!("Recipient's balance: {} {}", amount, &txn.denom);
+    Ok(())
+}
+
+async fn execute_txn(chain: &Cosmos, txn: &Txn, sender_wallet: &Wallet, recipient_address: &Address) -> Result<(), anyhow::Error> {
+    let amount_to_send: Vec<Coin> = vec![ Coin{denom: txn.denom.to_owned(), amount: txn.amount.to_string()} ];
+    sender_wallet.send_coins(&chain, *recipient_address, amount_to_send).await?;
+    Ok(())
+}
+
+async fn main_execution(txn: Txn) -> Result<(), anyhow::Error> {
+    // Connect to Cosmos chain
+    let chain: Cosmos = connect_to_chain().await?;
+
+    // Get Sender and Recipient info
+    let sender_info = get_sender_info().await?;
+    let recipient_address = get_recipient_address(&txn).await?;
+
+    // Before operation
+    get_sender_balance(&chain, &sender_info.address, &txn).await?;  // Get Sender balances and log them
+    get_recipient_balance(&chain, &recipient_address, &txn).await?;                   // Get Recipient balances and log them
+
+    // Execute transaction
+    execute_txn(&chain, &txn, &sender_info.wallet, &recipient_address).await?;
+
+    // After operation
+    get_sender_balance(&chain, &sender_info.address, &txn).await?;      // Get Sender balances and log them
+    get_recipient_balance(&chain, &recipient_address, &txn).await?;                   // Get Recipient balances and log them
+
+    Ok(())
 }
